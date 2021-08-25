1b42f7762a300ae39a4d1b1759a50438673087b2
Arvind Mewada <arvindmewada@Arvinds-MacBook-Air.local>
2021-08-25 20:28:44 +0200

    ready to testing

--- lib/Screens/FullLengthTestScreen/FLTTestDataPage.dart
+++ lib/Screens/FullLengthTestScreen/FLTTestDataPage.dart
@@ -2,11 +2,12 @@ import 'dart:convert';
 
 import 'package:elearning/MyStore.dart';
 import 'package:elearning/Screens/FullLengthTestScreen/FLTTestInstructionPage.dart';
+import 'package:elearning/constants.dart';
 import 'package:elearning/schemas/fltTestDataSchema.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
-import 'package:elearning/constants.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 // ignore_for_file: non_constant_identifier_names
@@ -15,6 +16,7 @@ class FLTTestDataPage extends StatefulWidget {
   final bool isHindi;
   final int exam_id;
   final int exam_id_2;
+
   const FLTTestDataPage(
       {Key? key,
       required this.isHindi,
@@ -67,82 +69,126 @@ class _FLTTestDataPageState extends State<FLTTestDataPage> {
                           borderRadius: BorderRadius.circular(20)),
                       child: Padding(
                         padding: const EdgeInsets.all(10),
-                        child: ListTile(
-                            trailing: TextButton(
-                                style: TextButton.styleFrom(
-                                    backgroundColor: kPrimaryColor,
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(20))),
-                                onPressed: () async {
-                                  await Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              FLTTestInstructionPage(
-                                                  isHindi: widget.isHindi,
-                                                  correctMarks: snapshot
-                                                      .data!
-                                                      .testData![index]
-                                                      .positiveMark!,
-                                                  totalQues: snapshot
-                                                      .data!
-                                                      .testData![index]
-                                                      .totalQues!,
-                                                  heading: snapshot
-                                                      .data!
-                                                      .testData![index]
-                                                      .testName!,
-                                                  reqPercentage: snapshot
-                                                      .data!
-                                                      .testData![index]
-                                                      .testReqPer!,
-                                                  incorrectMarks: snapshot
-                                                      .data!
-                                                      .testData![index]
-                                                      .negativeMark!,
-                                                  test_id: snapshot.data!
-                                                      .testData![index].testId!,
-                                                  title: widget.title,
-                                                  totalTime: snapshot
-                                                      .data!
-                                                      .testData![index]
-                                                      .totalTime!)));
-                                  if (mounted) setState(() {});
-                                },
-                                child: Text(
-                                  'Start',
-                                  style: TextStyle(color: Colors.white),
-                                )),
-                            subtitle: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    snapshot.data!.testData![index].testDesc!,
-                                  ),
+                        child: GestureDetector(
+                          onTap: () {
+                            snapshot.data!.testData![index].testStatus == 3
+                                ? Fluttertoast.showToast(
+                                    msg:
+                                        "This test is not activated. Please contact "
+                                        "your institute administrator to activate this test ")
+                                : null;
+                          },
+                          child: ListTile(
+                              trailing: TextButton(
+                                  style: TextButton.styleFrom(
+                                      backgroundColor: snapshot
+                                                  .data!
+                                                  .testData![index]
+                                                  .testStatus ==
+                                              1
+                                          ? kPrimaryColor
+                                          : Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20))),
+                                  onPressed: () async {
+                                    snapshot.data!.testData![index]
+                                                .testStatus ==
+                                            1
+                                        ? await Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    FLTTestInstructionPage(
+                                                        isHindi: widget.isHindi,
+                                                        correctMarks: snapshot
+                                                            .data!
+                                                            .testData![index]
+                                                            .positiveMark!,
+                                                        totalQues: snapshot
+                                                            .data!
+                                                            .testData![index]
+                                                            .totalQues!,
+                                                        heading: snapshot
+                                                            .data!
+                                                            .testData![index]
+                                                            .testName!,
+                                                        reqPercentage: snapshot
+                                                            .data!
+                                                            .testData![index]
+                                                            .testReqPer!,
+                                                        incorrectMarks: snapshot
+                                                            .data!
+                                                            .testData![index]
+                                                            .negativeMark!,
+                                                        test_id: snapshot
+                                                            .data!
+                                                            .testData![index]
+                                                            .testId!,
+                                                        title: widget.title,
+                                                        totalTime: snapshot
+                                                            .data!
+                                                            .testData![index]
+                                                            .totalTime!)))
+                                        : (){};
+                                    if (mounted) setState(() {});
+                                  },
+                                  child: snapshot.data!.testData![index]
+                                              .testStatus ==
+                                          1
+                                      ? Text(
+                                          'Start',
+                                          style: TextStyle(color: Colors.white),
+                                        )
+                                      : Text(
+                                          '',
+                                          style: TextStyle(color: Colors.white),
+                                        )),
+                              subtitle: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  snapshot.data!.testData![index].testStatus !=
+                                          2
+                                      ? Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: Text(
+                                            snapshot.data!.testData![index]
+                                                .testDesc!,
+                                          ),
+                                        )
+                                      : Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: Text(
+                                            "Coming soon",
+                                          ),
+                                        ),
+                                  Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Expanded(
+                                          child: Text(snapshot.data!
+                                                  .testData![index].totalQues! +
+                                              ' Questions'),
+                                        ),
+                                        SizedBox(width: 20),
+                                        Expanded(
+                                          child: Text(snapshot.data!
+                                                  .testData![index].totalTime! +
+                                              ' Minutes'),
+                                        ),
+                                      ])
+                                ],
+                              ),
+                              title: Text(
+                                snapshot.data!.testData![index].testName!,
+                                style: TextStyle(
+                                  color: getColorCode(snapshot
+                                      .data!.testData![index].testStatus),
                                 ),
-                                Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        child: Text(snapshot.data!
-                                                .testData![index].totalQues! +
-                                            ' Questions'),
-                                      ),
-                                      SizedBox(width: 20),
-                                      Expanded(
-                                        child: Text(snapshot.data!
-                                                .testData![index].totalTime! +
-                                            ' Minutes'),
-                                      ),
-                                    ])
-                              ],
-                            ),
-                            title: Text(
-                                snapshot.data!.testData![index].testName!)),
+                              )),
+                        ),
                       ),
                     ),
                   );
@@ -153,5 +199,17 @@ class _FLTTestDataPageState extends State<FLTTestDataPage> {
         },
       ),
     );
+  }
+
+  getColorCode(dynamic index) {
+    switch (index) {
+      case 1:
+        return Colors.blue;
+      case 2:
+        return Colors.blueGrey;
+      case 3:
+        return Colors.grey;
+      default:
+    }
   }
 }
